5728ec0e2ddcd37e5589e80dd9ae0466c027b5d4
= <=>
2021-01-26 21:22:38 -0300

    aplication implemented

---
+++ src/script.js
@@ -0,0 +1,36 @@
+const divResult = document.querySelector('#result');
+const button = document.querySelector('#button');
+const states = document.querySelector('#states');
+
+async function renderState() {
+    try {
+        const response = await axios.get(`https://covid19-brazil-api.now.sh/api/report/v1/brazil/uf/${states.value}`);
+        console.log(response.data);
+
+        const state = response.data.state;
+        const cases = response.data.cases;
+        const deaths = response.data.deaths;
+
+        divResult.innerHTML = `
+            <h1>${state}</h1>
+            <div>
+                <strong>Casos:</strong>
+                <span>${cases.toLocaleString('pt-br')}</span>
+            </div>
+            <div>
+                <strong>Mortes:</strong>
+                <span>${deaths.toLocaleString('pt-br')}</span>
+            </div>
+            `;
+        divResult.classList.remove('closed');
+
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+button.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    renderState();
+});
